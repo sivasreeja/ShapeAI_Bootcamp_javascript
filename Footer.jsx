ccc4ff47e569093f3960494abb82822171173149
sivasreeja <86600524+sivasreeja@users.noreply.github.com>
2021-06-28 15:43:25 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by shape ai @ {new Date().getFullYear()}
+      </p>
+      </footer>
+      
+  );
+}export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1 className="note">Javascript and React.js</h1>
+      <p className="note">A basic web dev React Js bootcamp </p>
+      </div>
+  );
+  }
+export default Info;
